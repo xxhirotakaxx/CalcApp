cd29ed074005b75c35f79e7cb029f30666e6b668
xxhirotakaxx <m.hirotaka0628@me.com>
2018-11-04 15:58:27 +0900

    例外処理の追加

--- app/src/main/java/com/example/hirotaka/calcapp/MainActivity.java
+++ app/src/main/java/com/example/hirotaka/calcapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hirotaka.calcapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,11 +39,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (mEditText1.length() == 0 || mEditText2.length() == 0) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("エラー");
+            alertDialogBuilder.setMessage("入力欄に空白があります。");
+
+            alertDialogBuilder.setPositiveButton("閉じる",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
             return;
         }
 
-        Double num1 = Double.parseDouble(mEditText1.getText().toString());
-        Double num2 = Double.parseDouble(mEditText2.getText().toString());
+        Double num1, num2;
+        try {
+            num1 = Double.parseDouble(mEditText1.getText().toString());
+            num2 = Double.parseDouble(mEditText2.getText().toString());
+        } catch (Exception e) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("エラー");
+            alertDialogBuilder.setMessage("数字を入力してください。");
+
+            alertDialogBuilder.setPositiveButton("閉じる",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            return;
+        }
+
         Intent intent = new Intent(this, SecondActivity.class);
         if (v.getId() == R.id.button1) {
             intent.putExtra("VALUE", num1 + num2);
@@ -49,6 +82,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (v.getId() == R.id.button3) {
             intent.putExtra("VALUE", num1 * num2);
         } else if (v.getId() == R.id.button4) {
+            if (num2 == 0) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("エラー");
+                alertDialogBuilder.setMessage("0での割り算はできません。");
+
+                alertDialogBuilder.setPositiveButton("閉じる",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+                return;
+            }
+
             intent.putExtra("VALUE", num1 / num2);
         }
         startActivity(intent);
